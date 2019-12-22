4e4112810bcce79be971c31375b3b6f8627ea878
CSNLP <csnlp16@126.com>
2019-12-22 14:39:27 +0800

    comment the panic in mem_init() function

--- kern/pmap.c
+++ kern/pmap.c
@@ -131,7 +131,7 @@ mem_init(void)
 	i386_detect_memory();
 
 	// Remove this line when you're ready to test this function.
-	panic("mem_init: This function is not finished\n");
+	//panic("mem_init: This function is not finished\n");
 
 	//////////////////////////////////////////////////////////////////////
 	// create initial page directory.
